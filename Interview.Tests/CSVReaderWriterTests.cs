9bc80e8f574f931fa036c68fea7ad851f0827d42
bohatermateusz <bohatermateusz@gmail.com>
2020-09-24 13:00:17 +0200

    Changes in code structure

--- Interview.Tests/CSVReaderWriterTests.cs
+++ Interview.Tests/CSVReaderWriterTests.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace Interview.Tests
 {
-    public class CSVReaderWriterTests
+    public class CsvReaderWriterTests
     {
         [SetUp]
         public void Setup()
@@ -14,18 +14,21 @@ namespace Interview.Tests
         [Test]
         public void WritesAndReadsFile()
         {
-            var readerWriter = new CSVReaderWriter();
+            var readerWriter = new CsvReaderWriter();
             var tempFileName = Path.GetTempFileName();
-            readerWriter.Open(tempFileName, CSVReaderWriter.Mode.Write);
+            readerWriter.Open(tempFileName, CsvReaderWriter.Mode.Write);
             for (int i = 0; i < 1000; i++)
             {
                 readerWriter.Write(i.ToString(), (i+1).ToString());
             }
             
-            readerWriter.Open(tempFileName, CSVReaderWriter.Mode.Read);
+            readerWriter.Open(tempFileName, CsvReaderWriter.Mode.Read);
 
+            
             List<(string Column1, string Column2)> columns = new List<(string Column1, string Column2)>();
-            while (readerWriter.Read(out var column1, out var column2))
+            string column1 = "dd";
+            string column2 = "aa";
+            while (readerWriter.Read(column1,column2))
             {
                 columns.Add((column1, column2));
             }
--- Interview/CSVReaderWriter.cs
+++ Interview/CSVReaderWriter.cs
@@ -10,33 +10,34 @@ namespace Interview
         private StreamWriter _writerStream;
 
         [Flags]
-        public enum Mode { Read = 1, Write = 2 };
+        public enum Mode { Read = 1, Write = 2 }
 
         public void Open(string fileName, Mode mode)
         {
-            if (mode == Mode.Read)
+            switch (mode)
             {
-                _readerStream = new StreamReader(File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
-            }
-            else if (mode == Mode.Write)
-            {
-                FileInfo fileInfo = new FileInfo(fileName);
-                _writerStream = new StreamWriter(fileInfo.Open(FileMode.Create, FileAccess.Write, FileShare.ReadWrite)) {AutoFlush = true};
-            }
-            else
-            {
-                throw new Exception("Unknown file mode for " + fileName);
+                case Mode.Read:
+                    _readerStream = new StreamReader(File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
+                    break;
+                case Mode.Write:
+                {
+                    var fileInfo = new FileInfo(fileName);
+                    _writerStream = new StreamWriter(fileInfo.Open(FileMode.Create, FileAccess.Write, FileShare.ReadWrite)) {AutoFlush = true};
+                    break;
+                }
+                default:
+                    throw new Exception("Unknown file mode for " + fileName);
             }
         }
 
         public void Write(params string[] columns)
         {
-            string outPut = "";
+            var outPut = "";
 
-            for (int i = 0; i < columns.Length; i++)
+            for (var i = 0; i < columns.Length; i++)
             {
                 outPut += columns[i];
-                if ((columns.Length - 1) != i)
+                if (columns.Length - 1 != i)
                 {
                     outPut += "\t";
                 }
@@ -47,15 +48,13 @@ namespace Interview
 
         public bool Read()
         {
-            var separator = '\t';
+            const char separator = '\t';
 
             var line = ReadLine();
             var columns = GetColumns(line, separator);
 
             if (columns.Length == 0)
             {
-       
-
                 return false;
             }
 
@@ -71,13 +70,13 @@ namespace Interview
         {
             return _readerStream.ReadLine();
         }
-        private string[] GetColumns(string line, char separator)
+        private static string[] GetColumns(string line, char separator)
         {
             var columns = new List<string>();
             int index;
             while ((index = line.IndexOf(separator)) > 0)
             {
-                string column = line.Substring(0, index);
+                var column = line.Substring(0, index);
                 line = line.Substring(index + 1);
                 columns.Add(column);
             }
